be71e3f6bae0eee0a4b5c72b05aa4a392d38e412
Karume-lab <danielkarume@outlook.com>
2024-11-24 22:38:24 +0300

    fix: :adhesive_bandage: fix sidebar issue

--- src/components/sidebar/index.ts
+++ src/components/sidebar/index.ts
@@ -1,5 +1,8 @@
-import { AdminSideBar, SHARED_SIDEBAR_ITEMS } from "./AdminSideBar";
+import {
+  AdminSideBar,
+  ADMIN_SELLER_SHARED_SIDEBAR_ITEMS,
+} from "./AdminSideBar";
 import SellerSideBar from "./SellerSideBar";
 import BuyerSideBar from "./BuyerSideBar";
 
-export { AdminSideBar, SellerSideBar, SHARED_SIDEBAR_ITEMS };
+export { AdminSideBar, SellerSideBar, BuyerSideBar, ADMIN_SELLER_SHARED_SIDEBAR_ITEMS };
