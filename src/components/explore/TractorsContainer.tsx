09b30b93b5b990c52120f2914b0a202f0fea3da5
Peachy-Njenga <peacheswagithi16@gmail.com>
2024-11-26 12:12:15 +0300

    style: :lipstick: improved tractor and attachment listing UI

--- src/components/explore/TractorsContainer.tsx
+++ src/components/explore/TractorsContainer.tsx
@@ -31,7 +31,13 @@ const TractorsContainer = () => {
   });
 
   if (status === "pending") {
-    return <p>Loading ...</p>;
+    return <div className="flex justify-center items-center space-x-2 w-full h-screen">
+    <div className="w-4 h-4 bg-primary rounded-full animate-pulse"></div>
+    <div className="w-4 h-4 bg-primary rounded-full animate-pulse"></div>
+    <div className="w-4 h-4 bg-primary rounded-full animate-pulse"></div>
+  </div>
+  
+  
   }
 
   if (status === "error") {
@@ -46,16 +52,18 @@ const TractorsContainer = () => {
 
   return (
     <InfiniteScrollContainer
-      className="flex"
+      className="p-4 "
       onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
     >
+      <div className="grid grid-cols-5 ">
       {tractors.map((tractor) => (
         <TractorCard key={tractor.id} tractor={tractor} />
       ))}
 
       {isFetchingNextPage && <Loader className="my-4" />}
 
-      {!hasNextPage && <p>You have reached the end</p>}
+      </div>
+      {!hasNextPage && <p className="text-center">You have reached the end of the page</p>}
     </InfiniteScrollContainer>
   );
 };
